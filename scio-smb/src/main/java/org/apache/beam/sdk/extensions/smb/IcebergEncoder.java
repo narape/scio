318e6efff4220471bb0e37c4b9eaf205c2ceacc3
Nelson Arapé <nelson@spotify.com>
2024-01-25 12:17:35 +0100

    Add tests for spec

--- scio-smb/src/main/java/org/apache/beam/sdk/extensions/smb/IcebergEncoder.java
+++ scio-smb/src/main/java/org/apache/beam/sdk/extensions/smb/IcebergEncoder.java
@@ -4,7 +4,14 @@ import io.airlift.slice.Slice;
 import io.airlift.slice.Slices;
 import org.apache.beam.sdk.coders.Coder;
 
-import java.time.*;
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.UUID;
 
@@ -42,8 +49,33 @@ public final class IcebergEncoder implements BucketMetadata.Encoder {
         return encode(value.toNanoOfDay() / 1000);
     }
 
+    private byte[] encode(LocalDateTime value) {
+        return encode(value.atOffset(ZoneOffset.UTC).toInstant());
+    }
+
+    private byte[] encode(ZonedDateTime value) {
+        return encode(value.toInstant());
+    }
+
     private byte[] encode(Instant value) {
         return encode(ChronoUnit.MICROS.between(EPOCH, value.atOffset(ZoneOffset.UTC)));
+    }
+
+    private byte[] encode(BigDecimal value) {
+        return value.unscaledValue().toByteArray();
+    }
+
+    private byte[] encode(boolean value) {
+        return encode((value) ? 1 : 0);
+    }
+
+    private byte[] encode(float value) {
+        return encode((double) value);
+    }
+
+    private byte[] encode(double value) {
+        double canonizedValue = value == -0D ? 0D : value;
+        return encode(Double.doubleToLongBits(canonizedValue));
     }
 
     @Override
@@ -69,8 +101,28 @@ public final class IcebergEncoder implements BucketMetadata.Encoder {
         if (value instanceof LocalTime) {
             return encode((LocalTime) value);
         }
+        if (value instanceof LocalDateTime) {
+            return encode((LocalDateTime) value);
+        }
+        if (value instanceof ZonedDateTime) {
+            return encode((ZonedDateTime) value);
+        }
         if (value instanceof Instant) {
             return encode((Instant) value);
+        }
+        if (value instanceof BigDecimal) {
+            return encode((BigDecimal) value);
+        }
+
+        // types below are not currently valid for bucketing but with a defined hash function
+        if (value instanceof Boolean) {
+            return encode((Boolean) value);
+        }
+        if (value instanceof Float) {
+            return encode((Float) value);
+        }
+        if (value instanceof Double) {
+            return encode((Double) value);
         }
 
         throw new UnsupportedOperationException("Unsupported type: " + value.getClass());
